0d4e25ea42d573030ea95e1807360af8e5c19f4e
chaser945 <113222684+chaser945@users.noreply.github.com>
2023-08-05 14:54:40 +0500

    fixed-logo-bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import Wrapper from "../assets/wrappers/Navbar"
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa"
-import Logo from "./logo"
+import Logo from "./Logo"
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { toggleBigSideBar, logOut } from "../features/user/userSlice"
